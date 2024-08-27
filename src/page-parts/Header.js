426f20e00da9cdf4db678d0f3c6c6192230783f3
Roman <lostspace666@gmail.com>
2024-08-27 19:26:18 +0300

    import export

---
+++ src/page-parts/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+      <header className='header'>
+        <ul>
+          <li><a href='/'>Home</a></li>
+          <li><a href='/contact'>Contact</a></li>
+          <li><a href='/blog'>Blog</a></li>
+        </ul>
+      </header>
+    );
+  }
+
+export default Header
---
+++ src/page-parts/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+      <ul>
+        <li>list item</li>
+        <li>list item</li>
+      </ul>
+    );
+  }
+
+export default Nav
