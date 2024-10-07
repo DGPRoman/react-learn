0f9025ecc8f19d7e8a38236e68429c19cb7dc9ac
ropel <ropel@smile.fr>
2024-10-07 19:44:57 +0300

    fix users

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -4,13 +4,27 @@ import './ProfileInfo.css'
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
+  let aboutMe = props.profile.aboutMe;
+  let fullName = props.profile.fullName;
   return (
     <div>
       <div className='profile-description'>
         <div className="profile-photo">
           <img src={props.profile.photos.large != null ? props.profile.photos.large : userDefaultImg} alt="" />
         </div>
+        {fullName && <div className='full-name'>{fullName}</div>}
+        {props.profile.lookingForAJob && <div className='looking-for-job'>{props.profile.lookingForAJobDescription}</div>}
         <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
+        {aboutMe && <div className='about-me'>{aboutMe}</div>}
+        <div className="contacts">
+        {Object.entries(props.profile.contacts).map(([key, value]) => (
+                value && (
+                    <div key={key}>
+                        <strong>{key}:</strong> <a href={value}>{value}</a>
+                    </div>
+                )
+            ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,33 +2,43 @@ import { NavLink } from 'react-router-dom';
 import userDefaultImg from '../../images/default-profile-s.png';
 import './users.css';
 
-let Users = (props) => {
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [];
-
+let Users = ({ totalUsersCount, pageSize, currentPage, onPageChanged, users, follow, unfollow, followInProgress }) => {
+    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+    const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
+    const visiblePages = [];
 
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
-    // todo remove
-    pages = pages.slice(0, 5);
+    let fromPage = Math.max(currentPage - 3, 1);
+    let toPage = Math.min(currentPage + 2, pagesCount);
+
+    if (fromPage > 1) {
+        visiblePages.push(1);
+    }
+    
+    visiblePages.push(...pages.slice(fromPage - 1, toPage));
+
+    if (toPage < pagesCount) {
+        visiblePages.push(pagesCount);
+    }
 
     return (
         <div>
             <div className="pagination">
                 <ul>
-                    {pages.map(p => {
+                    {visiblePages.map(p => {
                         return (
-                            <li key={`page${p}`} className={props.currentPage === p ? 'active' : ''}
-                                onClick={(e) => { props.onPageChanged(p); }}>
+                            <li key={`page${p}`} className={currentPage === p ? 'active' : ''}
+                                onClick={(e) => { onPageChanged(p); }}>
                                 <span>{p}</span>
                             </li>
                         )
                     })}
                 </ul>
             </div>
-            {props.users.map(u => {
+            {users.map(u => {
                 return (
                     <div key={u.id}>
                         <span>
@@ -39,18 +49,14 @@ let Users = (props) => {
                             </div>
                             <div>
                                 {u.followed
-                                    ? <button onClick={() => { props.unfollow(u.id) }}  disabled={props.followInProgress.some(id => id === u.id)}>Unfollow</button>
-                                    : <button onClick={() => { props.follow(u.id) }} disabled={props.followInProgress.some(id => id === u.id)}>Follow</button>}
+                                    ? <button onClick={() => { unfollow(u.id) }}  disabled={followInProgress.some(id => id === u.id)}>Unfollow</button>
+                                    : <button onClick={() => { follow(u.id) }} disabled={followInProgress.some(id => id === u.id)}>Follow</button>}
                             </div>
                         </span>
                         <span>
                             <span>
                                 <div>{u.name}</div>
                                 <div>{u.status}</div>
-                            </span>
-                            <span>
-                                {/* <div>{u.location.city}</div> */}
-                                {/* <div>{u.location.country}</div> */}
                             </span>
                         </span>
                     </div>
